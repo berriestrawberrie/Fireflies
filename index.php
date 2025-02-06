98b342afc6baf3d569fffd9742b6f7226b88b8a3
Brooke Brown <brooke.henriksson@outlook.com>
2025-02-06 12:52:52 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+/** 
+ * Main template file.
+ * 
+ * @package Fireflies
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WordPress Theme</title>
+</head>
+
+<body>
+    Hello word press!
+</body>
+
+</html>
